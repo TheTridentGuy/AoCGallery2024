88f61e4bf4554f7208e447d551c45c08b8b43af3
TheTridentGuy <aiden.bohlander@gmail.com>
2024-12-05 22:50:36 -0800

    furries :yum:

--- code/day6.py
+++ code/day6.py
@@ -1,115 +1,152 @@
 with open("in.txt", "r") as f:
     i = f.read()
-si = """47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
-
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47
+si = """....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
 """
 
 # Part 1:
 def p1(i):
     """Part 1"""
-    def check_page(page, rules):
-        for rule in rules:
+    map1 = [list(line) for line in i.split("\n")]
+    for y in range(len(map1)):
+        for x in range(len(map1[y])):
+            if map1[y][x] == "^":
+                guardy = y
+                guardx = x
+                break
+    def move(x,y,map3,dirx,diry):
+        try:
+            if x+dirx < 0 or x+dirx >= len(map3[0]):
+                return False
+            elif y+diry < 0 or y+diry >= len(map3):
+                return False
+            elif map3[y+diry][x+dirx] == "#":
+                return "r"
+            else:
+                return "m"
+        except:
+            return False
+
+    def trace(x, y, map2):
+        actions = {
+            "^": (0, -1),
+            ">": (1, 0),
+            "v": (0, 1),
+            "<": (-1, 0),
+        }
+        lsact = ["^", ">", "v", "<"]
+        lsact.reverse()
+        action = move(x, y, map2, *actions[map2[y][x]])
+        if not action:
+            return False, None, None, map2
+        elif action == "r":
+            if map2[y][x] == "x":
+                return "l", x, y, map2
+            map2[y][x]=lsact[lsact.index(map2[y][x]) - 1]
+            return True, x, y, map2
+        else:
+            act = map2[y][x]
+            map2[y][x] = "x"
+            y = y+actions[act][1]
+            x = x+actions[act][0]
+            map2[y][x] = act
+            return True, x, y, map2
+
+    while True:
+        sum = 0
+        status, guardx, guardy, map1 = trace(guardx, guardy, map1)
+        if not status:
+            for y in range(len(map1)):
+                for x in range(len(map1[y])):
+                    if map1[y][x] == "x":
+                        sum+=1
+            return sum+1
+
+def p2(i):
+    """Part 2"""
+    map1 = [list(line) for line in i.split("\n")]
+    def p1for2(map1):
+        guardy1 = 0
+        guardx1 = 0
+        for y in range(len(map1)):
+            for x in range(len(map1[y])):
+                if map1[y][x] == "^":
+                    guardy1 = y
+                    guardx1 = x
+                    break
+        def move(x,y,map3,dirx,diry):
             try:
-                if not page.index(rule[0]) < page.index(rule[1]):
+                if x+dirx < 0 or x+dirx >= len(map3[0]):
                     return False
-            except ValueError:
-                pass
-        return True
+                elif y+diry < 0 or y+diry >= len(map3):
+                    return False
+                elif map3[y+diry][x+dirx] == "#":
+                    return "r"
+                else:
+                    return "m"
+            except:
+                return False
+
+        def trace(x, y, map2):
+            actions = {
+                "^": (0, -1),
+                ">": (1, 0),
+                "v": (0, 1),
+                "<": (-1, 0),
+            }
+            lsact = ["^", ">", "v", "<"]
+            lsact.reverse()
+            action = move(x, y, map2, *actions[map2[y][x]])
+            lastr = False
+            if not action:
+                return False, None, None, map2
+            elif action == "r":
+                lastr = True
+                map2[y][x]=lsact[lsact.index(map2[y][x]) - 1]
+                return True, x, y, map2
+            else:
+                act = map2[y][x]
+                map2[y][x] = "x"
+                y = y+actions[act][1]
+                x = x+actions[act][0]
+                if lastr and map2[y][x] == "x":
+                    return "l", None, None, map2
+                map2[y][x] = act
+                lastr = False
+                return True, x, y, map2
+        iters = 0
+        while True:
+            status, guardx1, guardy1, map1 = trace(guardx1, guardy1, map1)
+            if status == "l" or iters > 17030:
+                return "l"
+            elif not status:
+                return None
+            iters += 1
     sum = 0
-    i = i.split("\n\n")
-    rules = i[0].split("\n")
-    pages = i[1].split("\n")
-    for i in range(len(rules)):
-        rules[i] = rules[i].split("|")
-    for page in pages:
-        if check_page(page, rules):
-            #print(page, rules)
-            page = page.split(",")
-            if not page[0]:
-                continue
-            #print(page, "middle: ", int(page[int(len(page)/2)]))
-            sum += int(page[int(len(page)/2)])
+    import copy
+    for y in range(len(map1)):
+        for x in range(len(map1[y])):
+            if map1[y][x] == ".":
+                try:
+                    print("working new map")
+                    newmap = copy.deepcopy(map1)
+                    newmap[y][x] = "#"
+                    sum += 1 if p1for2(newmap) == "l" else 0
+                except:
+                    pass
     return sum
+
 
 
 print(p1(i))
 print("Sample Input:", p1(si))
-def p2(i):
-    """Part 2"""
-    def check_page(page, rules):
-        for rule in rules:
-            try:
-                if not page.index(rule[0]) < page.index(rule[1]):
-                    return False
-            except ValueError:
-                pass
-        return True
-    def fix_page(page: str, rules):
-        page = page.split(",")
-        page2 = page.copy()
-        for rule in rules:
-            try:
-                i1 = page.index(rule[0])
-                i2 = page.index(rule[1])
-                if i1 < i2:
-                    continue
-                page2[i1] = page[i2]
-                page2[i2] = page[i1]
-                page = page2.copy()
-                #print(rule, i1, i2, page, page2)
-            except ValueError:
-                pass
-        return page
-    sum = 0
-    i = i.split("\n\n")
-    rules = i[0].split("\n")
-    pages = i[1].split("\n")
-    for i in range(len(rules)):
-        rules[i] = rules[i].split("|")
-    for page in pages:
-        if check_page(page, rules):
-            continue
-        else:
-            #print(page, end=" | ")
-            page = ",".join(fix_page(page, rules))
-            page = ",".join(fix_page(page, rules))
-            page = ",".join(fix_page(page, rules))
-            page = ",".join(fix_page(page, rules))
-            page = ",".join(fix_page(page, rules))
-            page = ",".join(fix_page(page, rules))
-            page = ",".join(fix_page(page, rules))
-            page = fix_page(page, rules)
-            #print(page, sum, int(page[int(len(page)/2)]))
-            sum += int(page[int(len(page)/2)])
-    return sum
-
-
-
 print(p2(i))
 print("Sample Input:", p2(si))
