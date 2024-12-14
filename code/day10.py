fe7c06013f3ba08a20cbda85e136f7f0a115904d
TheTridentGuy <aiden.bohlander@gmail.com>
2024-12-14 14:03:20 -0800

    update latest solutions _meowwwwww_

---
+++ code/day10.py
@@ -0,0 +1,19 @@
+with open("in.txt", "r") as f:
+    i = f.read()
+si = """
+
+"""
+
+# Part 1:
+def p1(i):
+    """Part 1"""
+
+
+print(p1(i))
+print("Sample Input:", p1(si))
+def p2(i):
+    """Part 2"""
+
+
+print(p2(i))
+print("Sample Input:", p2(si))
---
+++ code/day12.py
@@ -0,0 +1,17 @@
+with open("in.txt", "r") as f:
+    i = f.read()
+si = """"""
+
+# Part 1:
+def p1(i):
+    """Part 1"""
+
+
+print("Sample Input:", p1(si))
+print(p1(i))
+def p2(i):
+    """Part 2"""
+
+
+print("Sample Input:", p2(si))
+print(p2(i))
