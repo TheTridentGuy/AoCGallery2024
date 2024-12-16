a10680bee5ba9b1acefacf2c1b15133908640607
TheTridentGuy <aiden.bohlander@gmail.com>
2024-12-15 22:03:58 -0800

    kinda did it in chatgpt today, so i could feed my furry rp addiction while doing it

---
+++ code/day16.py
@@ -0,0 +1,21 @@
+import functools
+import itertools
+import collections
+from pprint import pp, pprint
+import re
+with open("in.txt", "r") as f:
+    i = f.read()
+si = """"""
+
+# Part 1:
+def p1(i):
+    """Part 1"""
+
+print("Sample Input:", p1(p1si := si))
+print(p1i := p1(i))
+def p2(i):
+    """Part 2"""
+
+
+print("Part 2 - Sample Input:", p2si := p2(si))
+print("", p2i := p2(i))
